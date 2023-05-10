4610e09ad3520ace250d52f1a24b65dc664b6269
epiksilver24 <saul.cespedes19@gmail.com>
2023-05-10 17:48:16 +0200

    strstr finish

--- ring00/Libft/funciones/ft_strnstr.c
+++ ring00/Libft/funciones/ft_strnstr.c
@@ -6,11 +6,9 @@
 /*   By: scespede <scespede@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 11:41:59 by scespede          #+#    #+#             */
-/*   Updated: 2023/05/10 13:42:24 by scespede         ###   ########.fr       */
+/*   Updated: 2023/05/10 17:44:47 by scespede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 
 #include "libft.h"
 #include <stddef.h>
@@ -28,27 +26,30 @@ char 	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	count = 0;
 	str = (char *) haystack;
 	ned = (char *) needle;
+	if (ned[count] == '\0') {
+		return (char *) haystack;
+	}
 	while (str[count] && count < len)
 	{
-		position = 0;
-		if(str[count] == ned[position])
+		if(str[count] == ned[position] )
 		{
-			while (str[count + position] == ned[position])
+			while (str[count + position] == ned[position] && ned[position] != '\0' && (count + position) < len)
 			{
-				if (ned[position] != '\0')
-					return &str[count + position];
+				if (ned[position + 1] == '\0')
+					return &str[count];
 				position++;
 			}
+			position = 0;
 		}	
 		count++;
 	}
-	return ((char *)haystack);
+	return (NULL);
 }
-int main ()
-{
-  char  a[] = "lorem impsum dolor sit amet";
-  char	b[] = "impsum";
-  printf("mi funcionsadas\nnum= %s", ft_strnstr(a,b,15) );
-
-
-}
+//int main ()
+//{
+//  char  a[] = "lorem impsum dolor sit amet";
+//  char	b[] = "impsum";
+//  printf("mi funcionsadas\nnum= %s", ft_strnstr(a,b,15) );
+//
+//
+//}
