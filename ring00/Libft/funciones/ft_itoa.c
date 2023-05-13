9fa0ec2be0cc80c350032a089d11d6fc1c86575a
epiksilver24 <saul.cespedes19@gmail.com>
2023-05-13 16:57:17 +0200

    norminette itoa finish

--- ring00/Libft/funciones/ft_itoa.c
+++ ring00/Libft/funciones/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: epik </var/spool/mail/epik>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 14:16:10 by epik              #+#    #+#             */
-/*   Updated: 2023/05/13 16:31:56 by scespede         ###   ########.fr       */
+/*   Updated: 2023/05/13 16:50:09 by scespede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int lengn (int n)
+static int	lengn(int n)
 {
-	int count;
-	count = 0;
-	
-	if (n < 0) {
-	n *= -1;	
-	}
- while (n > 0) {
-  n /= 10;
-  count++;
- }
- return count;
-}
-char	*ft_itoa(int n)
-{
-	int	copyn;
-	size_t count;
-	char	*str;
+	int	count;
 
 	count = 0;
+	if (n < 0)
+	{
+		n *= -1;
+		count++;
+	}
+	while (n > 0)
+	{
+	n /= 10;
+	count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	int		copyn;
+	size_t	count;
+	char	*str;
+
 	if (n == -2147483648)
-		return ft_strdup("-2147483648");
+		return (ft_strdup("-2147483648"));
 	count = lengn(n);
 	if (count == 0)
-		return ft_strdup("0");
+		return (ft_strdup("0"));
 	if (n < 0)
-  	{
-		count++;
 		copyn = n * -1;
-  	}else
+	else
 		copyn = n;
 	str = malloc((sizeof(char)) * (count + 1));
 	if (!str)
@@ -58,12 +59,14 @@ char	*ft_itoa(int n)
 	}
 	if (n < 0)
 		str[0] = '-';
-	return str;
+	return (str);
 }
-//int main ()
-//{
-//  char *str;
-//  str =NULL;
-//  str =ft_itoa(-2147483648);
-//  printf("valor de los numero %s",str);
-//}
+/*
+int main ()
+{
+  char *str;
+  str =NULL;
+  str =ft_itoa(-214483648);
+  printf("valor de los numero %s",str);
+}
+*/
