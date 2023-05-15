1ac15738fc0b342c8670f6551fe08014a9fabd38
epiksilver24 <saul.cespedes19@gmail.com>
2023-05-15 23:51:30 +0200

    70% split

--- ring00/Libft/Tests/ft_split.c
+++ ring00/Libft/Tests/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: scespede <scespede@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 09:13:28 by scespede          #+#    #+#             */
-/*   Updated: 2023/05/15 14:23:42 by scespede         ###   ########.fr       */
+/*   Updated: 2023/05/15 23:48:43 by scespede         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,56 +41,67 @@ static size_t numword(const char *s, char sep)
 	}
 	return worlds;
 }
-int	countworld(const char *s, char c)
+static int	countworld(const char *s, char c)
 {
 	int count;
 
 	count = 0;
-	while (!s && *s != c) {
+	while (*s && *s != c) {
 		count++;
+		s++;
 	}
 	return (count);
 }
-
+static char  *maximumfree(char **str, size_t poss)
+{
+	while (poss-- > 0) {
+		free(str[poss]);
+	}
+	free(str);
+	return NULL;
+}
 char	**ft_split(char const *s, char c)
 {
 	char **ptr;
-	int	count;
-	int	indi;
-	
+	size_t	count;
+	size_t	worlds;
 
+	worlds = numword(s, c);
 	count = 0;
 	ptr = (char **)malloc(sizeof(char *) * (numword(s,c ) + 1));
-	if(!ptr)
-		return NULL;
-	while (count < (numword(s, c)))
+	if(ptr == NULL)
 	{
-		indi = 0;
-		while (*s == c && !s)
+		maximumfree(ptr, count);
+		return NULL;
+	}
+	while (count < worlds)
+	{
+		while (*s == c && *s)
 			s++;
 		ptr[count] = malloc(sizeof(char) * (countworld(s, c) + 1));
-		while (indi < (countworld(s, c))
-		{
-			
-		}
-		
+		if (ptr[count] == NULL)
+			return (char **) maximumfree(ptr, count);
+		if (!(ptr[count] = ft_substr(s, 0, countworld(s, c))))
+			return (char **) maximumfree(ptr, count);
+		s += countworld(s, c);
+		count++;	
 	}
 
-
+	ptr[count] = (char*)'\0';
 	return ptr;	
 }
-
+/*
 int main ()
 { 
-	//char a[] = "pokemon digimon  monster rancher   hunter  ";
-	char b[] = "1 2 3 4 5 6 7 8 9 0 ";
+	char b[] = "";
+//	char b[] = "1 2 3 4 5 6 7 8 9 0 ";
 	char **ptr;
-
 	ptr = ft_split(b,' ');
 	
-	printf("\nprimer indice\n %s",ptr[0]);
-	printf("\nprimer indice\n %s",ptr[1]);
-	printf("\nprimer indice\n %s",ptr[2]);
-	printf("\nprimer indice\n %s",ptr[3]);
-	printf("\nprimer indice\n %s",ptr[4]);
+	while (*ptr) {
+	
+	printf("\nprimer indice\n%s",*ptr);
+	ptr++;
+	}
 }
+*/
