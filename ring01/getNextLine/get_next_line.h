70050a4b264adfd1484132941e703533d3f52c71
Saul Cespedes Ribera <scespede@cbr15s4.42barcelona.com>
2023-06-26 17:07:41 +0200

    finish bor2beroot

---
+++ ring01/getNextLine/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scespede <scespede@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/26 12:56:05 by scespede          #+#    #+#             */
+/*   Updated: 2023/06/26 13:52:21 by scespede         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+#include <stdio.h>
+#include <unistd.h>
+
+char *get_next_line(int fd);
+
+#endif
+
